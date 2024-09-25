ce6213bc81f89551653f31c9a49ebefc0246829e
pasecolra <paulcolmenare@colegioguanenta.edu.co>
2024-09-25 18:42:20 -0500

    bd e imagenes

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>zapateria</title>
+</head>
+<body>
+    <h1>Zapateria la sangil</h1>
+    <?php
+    echo "hola";
+    ?>
+</body>
+</html>
